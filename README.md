cdcbbf0ae3464f5ba96e306d6b4ad4486372c8e7
Flávio Caetano <flavio@vieiracaetano.com>
2016-12-17 13:15:57 -0200

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+ToiLite
+=====
+
+A night light that lights up your toilette when you enter your restroom. Inspired by [IllumiBowl](https://illumibowl.com/).
+
+
+### Partlist:
+. 1 ATTiny85
+. 1 RGB LED
+. 1 LDR Sensor
+. 1 PIR Sensor
+. 1 Push button
+. 1 Timpot (10k)
+. 2 100R Resistor
+. 1 150R Resistor
+
+![Schematics](https://github.com/fjcaetano/ToiLite/raw/master/toilite_schem.png)
+
+The schematics and prototyping are using an ATTiny85 as microcontroller for the final product, but it can be easily exchanged for an Arduino board for tests.
+
+
+### TODO
+- [ ] Hook up the PIR
+- [ ] Wire it up in a perfboard
+- [ ] Make a housing for the product
+- [ ] Install on a toilette
---
+++ RGB.ino
@@ -0,0 +1,104 @@
+/*
+Adafruit Arduino - Lesson 3. RGB LED
+*/
+
+typedef struct RGB {
+  byte r;
+  byte g;
+  byte b;
+};
+
+RGB rgb(int h);
+void setColor(RGB rgb);
+
+const int redPin = 4; // PIN 3
+const int bluePin = 0; // PIN 5
+const int greenPin = 1; // PIN 6
+
+const int BUTTON = 2; // PIN 7
+int buttonPreviousRead = LOW;
+int isFixed = LOW;
+
+RGB color;
+
+//int POT = A0;
+//int LDR = A1;
+
+void setup()
+{
+//  Serial.begin(9600);
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+  pinMode(BUTTON, INPUT);
+//  pinMode(POT, INPUT);
+//  pinMode(LDR, INPUT);
+}
+
+void loop()
+{
+  for (int i = 0; i < 3600; i++) {
+    int button = digitalRead(BUTTON);
+//    int ldr = analogRead(LDR);
+//    int pot = analogRead(POT);
+
+    if (button == HIGH && buttonPreviousRead != HIGH) {
+      isFixed = !isFixed;
+    }
+
+    buttonPreviousRead = button;
+
+//    if (ldr < pot) {
+      if (!isFixed) {
+        color = rgb(i/10);
+      }
+      
+      setColor(color);
+//    }
+//    else {
+//      setColor({0, 0, 0});
+//    }
+
+    delay(10);
+  }
+}
+
+void setColor(RGB rgb)
+{
+  analogWrite(redPin, rgb.r);
+  analogWrite(greenPin, rgb.g);
+  analogWrite(bluePin, rgb.b);
+}
+
+
+RGB rgb(int h)
+{
+  int angle = (h >= 360 ? 0 : h);
+  float sector = float(angle) / 60; // Sector
+  int i = floor(sector);
+  float f = sector - i; // Factorial part of h
+
+  float q = (1 - f);
+
+  switch (i) {
+    case 0:
+      return {0xff, int(f * 0xff), 0};
+
+    case 1:
+      return {int(q * 0xff), 0xff, 0};
+
+    case 2:
+      return {0, 0xff, int(f * 0xff)};
+
+    case 3:
+      return {0, int(q * 0xff), 0xff};
+
+    case 4:
+      return {int(f * 0xff), 0, 0xff};
+
+    default:
+      return {0xff, 0, int(q * 0xff)};
+  }
+}
