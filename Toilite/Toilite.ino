962d6a6e0523729b1871c7b4022d02983d23ee55
Flávio Caetano <flavio@vieiracaetano.com>
2018-03-14 10:11:32 -0300

    WIP

--- Toilite/Toilite.ino
+++ Toilite/Toilite.ino
@@ -6,33 +6,40 @@
 
 #include <EEPROM.h>
 
+#define RED_PIN PB4 // PIN 3
+#define GREEN_PIN PB1 // PIN 6
+#define BLUE_PIN PB0 // PIN 5
+
+#define BUTTON PB2 // PIN 7
+
+#define MAX_ANGLE 256
+
 typedef struct RGB {
-  byte r;
-  byte g;
-  byte b;
+  char r;
+  char g;
+  char b;
 };
 
-RGB rgb(int h);
-void setColorFromAngle(int angle);
+RGB rgb(char h);
+void setColorFromAngle(char angle);
 void button_pressed();
-void loop_colors(int startAngle);
+void loop_colors(char startAngle);
 
-const int RED_PIN = 4; // PIN 3
-const int GREEN_PIN = 1; // PIN 6
-const int BLUE_PIN = 0; // PIN 5
-
-const int BUTTON = 2; // PIN 7
-
-int currentAngle;
+char currentAngle;
 bool shouldStopLooping = false;
 
 void setup()
 {
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
+//  pinMode(RED_PIN, OUTPUT);
+//  pinMode(GREEN_PIN, OUTPUT);
+//  pinMode(BLUE_PIN, OUTPUT);
 
-  pinMode(BUTTON, INPUT);
+  // Setting output mode for pins
+  DDRB |= (1 << RED_PIN);
+  DDRB |= (1 << GREEN_PIN);
+  DDRB |= (1 << BLUE_PIN);
+
+//  pinMode(BUTTON, INPUT);
 
   randomSeed(analogRead(0));
 
@@ -41,16 +48,13 @@ void setup()
   PCMSK = 0b00000100;    // turn on interrupts on pins PB2
   sei();                 // enables interrupts
 
-  int addr0 = EEPROM.read(0);
-  int addr1 = EEPROM.read(1);
-
-  currentAngle = (addr0 + (addr1 << 8));
+  currentAngle = EEPROM.read(0);
   setColorFromAngle(currentAngle);
 
-  attachInterrupt(digitalPinToInterrupt(BUTTON), button_pressed, RISING);
+  attachInterrupt(BUTTON, button_pressed, RISING);
 
   if (currentAngle == 0) {
-    loop_colors(random(360));
+    loop_colors(random(MAX_ANGLE));
   }
 }
 
@@ -60,31 +64,29 @@ void loop() {
 
 void button_pressed() {
   if (EEPROM.read(0) == 0) {
-    int angle = currentAngle;
-    EEPROM.write(0, angle % 256);
-    EEPROM.write(1, angle >> 8);
+    char angle = currentAngle;
+    EEPROM.write(0, angle);
 
     shouldStopLooping = true;
   }
   else {
     // Clears EEPROM
     EEPROM.write(0, 0);
-    EEPROM.write(1, 0);
 
     loop_colors(currentAngle);
   }
 }
 
-void loop_colors(int startAngle) {
+void loop_colors(char startAngle) {
   shouldStopLooping = false;
 
-  for (int i = startAngle; i < 360; i++) {
+  for (char i = startAngle; i < MAX_ANGLE; i++) {
     if (shouldStopLooping) {
       return;
     }
 
     currentAngle = i;
-  	setColorFromAngle(i);
+    setColorFromAngle(i);
 
     delay(100);
   }
@@ -92,41 +94,50 @@ void loop_colors(int startAngle) {
   loop_colors(0);
 }
 
-void setColorFromAngle(int angle)
+void setColorFromAngle(char angle)
 {
   RGB color = rgb(angle);
-  analogWrite(RED_PIN, color.r);
-  analogWrite(GREEN_PIN, color.g);
-  analogWrite(BLUE_PIN, color.b);
+
+ analogWrite(RED_PIN, color.r);
+//  sbi(TCCR0A, COM0A1);
+//  OCR0A = color.r;
+
+//  analogWrite(GREEN_PIN, color.g);
+//  sbi(TCCR0A, COM0B1);
+//  OCR0B = color.g;
+
+//  analogWrite(BLUE_PIN, color.b);
+//  sbi(TCCR0A, COM0A1);
+//  OCR0A = color.b;
 }
 
 
-RGB rgb(int h)
+RGB rgb(char angle)
 {
-  int angle = (h >= 360 ? 0 : h);
-  float sector = float(angle) / 60; // Sector
-  int i = floor(sector);
+  float sector = float(angle % MAX_ANGLE) / float(MAX_ANGLE / 6); // Sector
+  char i = floor(sector);
   float f = sector - i; // Factorial part of h
 
   float q = (1 - f);
 
   switch (i) {
     case 0:
-      return {0xff, int(f * 0xff), 0};
+      return {0xff, char(f * 0xff), 0};
 
     case 1:
-      return {int(q * 0xff), 0xff, 0};
+      return {char(q * 0xff), 0xff, 0};
 
     case 2:
-      return {0, 0xff, int(f * 0xff)};
+      return {0, 0xff, char(f * 0xff)};
 
     case 3:
-      return {0, int(q * 0xff), 0xff};
+      return {0, char(q * 0xff), 0xff};
 
     case 4:
-      return {int(f * 0xff), 0, 0xff};
+      return {char(f * 0xff), 0, 0xff};
 
     default:
-      return {0xff, 0, int(q * 0xff)};
+      return {0xff, 0, char(q * 0xff)};
   }
 }
+
